6450e0da46a4361e96a44cad7bd5bbd9ea734b33
Mohammed Parves <mdperrfan@gmail.com>
2025-02-06 23:48:59 +0600

    Files and Folder structured and students routes

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,12 +1,28 @@
 import React from 'react'
-
+import { Routes, Route } from 'react-router-dom'
+import Home from './pages/student/Home.jsx'
+import CourseList from './pages/student/CourseList'
+import CourseDetails from './pages/student/CourseDetails'
+import MyEnrollments from './pages/student/MyEnrollments'
+import Player from './pages/student/Player'
+import Loading from './components/students/Loading.jsx'
 const App = () => {
   return (
-    <div>
-      <h1 className="text-3xl font-bold underline">
-        Hello world!
-      </h1>
-    </div>
+    <>
+      <Routes>
+        {/*Routes for student*/}        
+        <Route path='/' element={<Home/>} />
+        <Route path='/course-list' element={<CourseList/>} />
+        <Route path='/course-list/:input' element={<CourseList/>} />
+        <Route path='/course/:id' element={<CourseDetails/>} />
+        <Route path='/my-enrollents' element={<MyEnrollments/>} />
+        <Route path='/player/:courseId' element={<Player/>} />
+        <Route path='/loading/:path' element={<Loading/>} />
+        {/* Routes for educator */}
+
+        
+      </Routes>
+    </>
   )
 }
 
