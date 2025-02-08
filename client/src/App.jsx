31eee3da83d96456ab170cdb54770b4e850169a3
Mohammed Parves <mdperrfan@gmail.com>
2025-02-08 21:12:35 +0600

    Added educator routes and made navbar

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,14 +1,22 @@
 import React from 'react'
-import { Routes, Route } from 'react-router-dom'
+import { Routes, Route, useMatch } from 'react-router-dom'
 import Home from './pages/student/Home.jsx'
 import CourseList from './pages/student/CourseList'
 import CourseDetails from './pages/student/CourseDetails'
 import MyEnrollments from './pages/student/MyEnrollments'
 import Player from './pages/student/Player'
 import Loading from './components/students/Loading.jsx'
+import Educator from './pages/educator/Educator.jsx'
+import Dashboard from './pages/educator/Dashboard.jsx'
+import MyCourses from './pages/educator/MyCourses.jsx'
+import AddCourse from './pages/educator/AddCourse.jsx'
+import StudentsEnrolled from './pages/educator/StudentsEnrolled.jsx'
+import Navbar from './components/students/Navbar.jsx'
 const App = () => {
+  const isEducatorRoute = useMatch('/educator/*');
   return (
-    <>
+    <div className='text-default min-h-screen bg-white'>
+      {!isEducatorRoute && <Navbar/>}
       <Routes>
         {/*Routes for student*/}        
         <Route path='/' element={<Home/>} />
@@ -19,10 +27,15 @@ const App = () => {
         <Route path='/player/:courseId' element={<Player/>} />
         <Route path='/loading/:path' element={<Loading/>} />
         {/* Routes for educator */}
-
+        <Route path='/educator' element={<Educator/>}>
+          <Route path='educator' element={<Dashboard/>} />
+          <Route path='my-courses' element={<MyCourses/>} />
+          <Route path='add-course' element={<AddCourse/>} />
+          <Route path='student-enrolled' element={<StudentsEnrolled/>} />
+        </Route>
         
       </Routes>
-    </>
+    </div>
   )
 }
 
--- client/src/components/students/Navbar.jsx
+++ client/src/components/students/Navbar.jsx
@@ -1,9 +1,27 @@
 import React from 'react'
-
+import { assets } from '../../assets/assets'
+import { Link } from 'react-router-dom'
 const Navbar = () => {
+  const isCourseListPage = location.pathname.includes('/course-list')
   return (
-    <div>
-      <h1>Navbar</h1>
+    <div className={`flex items-center justify-between border-b border-gray-500 py-4 px-4 sm:px-10 md:px-14 lg:px-36 ${isCourseListPage ? 'bg-white':'bg-cyan-100/70'}`}>
+      <img src={assets.logo} alt="" className='w-28 lg:w-32 cursor-pointer'/>
+      <div className='hidden md:flex items-center gap-5 text-gray-500'>
+          <div className='flex items-center gap-4'>
+            <button >Become Educator</button>
+            |
+            <Link to="/my-enrollments"> My Enrollments</Link>
+          </div>
+          <button className='bg-blue-600 px-5 py-2 rounded-full text-white '>Create account</button>
+      </div>
+      <div className='md:hidden flex items-center gap-2 sm:gap-5 text-gray-500'>
+          <div className='flex items-center gap-2'>
+          <button >Become Educator</button>
+            |
+            <Link to="/my-enrollments"> My Enrollments</Link>
+            <button><img src={assets.user_icon} alt="" /></button>
+          </div>
+      </div>
     </div>
   )
 }
