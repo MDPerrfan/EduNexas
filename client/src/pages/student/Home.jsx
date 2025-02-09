b01235739eca9260c78d5cd7f1dc082cf4719ee6
Mohammed Parves <mdperrfan@gmail.com>
2025-02-10 00:07:19 +0600

    Added clerk auth and hero

--- client/src/pages/student/Home.jsx
+++ client/src/pages/student/Home.jsx
@@ -1,9 +1,11 @@
 import React from 'react'
+import Hero from '../../components/students/Hero'
 
 const Home = () => {
   return (
     <div>
-      <h1>Home</h1>
+      <Hero/>
+      <h1></h1>
     </div>
   )
 }
