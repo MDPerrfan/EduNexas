1a9d079284036cb2927e152ce49d213ba6884617
Mohammed Parves <mdperrfan@gmail.com>
2025-05-27 21:25:47 +0600

    CourseList page is completed

--- client/src/components/students/Footer.jsx
+++ client/src/components/students/Footer.jsx
@@ -7,11 +7,11 @@ const Footer = () => {
       <div className="flex flex-col md:flex-row items-start px-8 md:px-0 justify-center gap-10 md:gap-32 py-10 border-b border-white/30 text-gray-300">
         <div className="flex flex-col md:items-start items-center w-full">
           <img className='w-44' src={assets.logo3} alt="" />
-          <p className='text-base py-3'>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Quisquam odit aliquid amet, minus accusantium fugit. Sequi molestias, doloribus incidunt tempore doloremque </p>
+          <p className='text-base py-3'>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Quisquam odit aliquid amet, minus accusantium fugit. Sequi molestias</p>
         </div>
         <div className="flex flex-col md:items-start items-center w-full">
           <h1 className='text-lg text-white font-medium pb-3'>Company</h1>
-          <ul className='flex flex-col gap-1'>
+          <ul className='flex md:flex-col flex-row gap-2'>
             <li>Home</li>
             <li>About us</li>
             <li>Contact</li>
@@ -19,8 +19,8 @@ const Footer = () => {
           </ul>
         </div>
         <div className="flex flex-col md:items-start items-center w-full">
-          <h1 className='text-lg text-white font-medium pb-3'>Subcribe to our newsletter</h1>
-          <p className='text-base py-3'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Nulla, ab?</p>
+          <h1 className='text-lg text-white font-medium pb-1'>Subcribe to our newsletter</h1>
+          <p className='text-base'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Nulla, ab?</p>
           <div className='flex gap-3 my-2'>
             <input className='py-1 px-3 rounded-sm bg-gray-600' type="text" placeholder='Enter your email' />
             <button className='bg-[coral] px-5 py-2 rounded-md'>Subscribe</button>
--- client/src/pages/student/CourseList.jsx
+++ client/src/pages/student/CourseList.jsx
@@ -1,10 +1,50 @@
-import React from 'react'
+import React, { useContext, useEffect, useState } from 'react'
+import { AppContext } from '../../context/AppContext'
+import SearchBar from '../../components/students/SearchBar'
+import CourseCard from '../../components/students/CourseCard'
+import Footer from '../../components/students/Footer'
+import { useParams } from 'react-router-dom'
 
 const CourseList = () => {
+  const { allcourses, navigate } = useContext(AppContext)
+  const { input } = useParams()
+  const [filteredCourse, setFilteredcourse] = useState([])
+  useEffect(() => {
+    if(allcourses && allcourses.length>0){
+      const tempCourses = allcourses.slice()
+      input ? 
+      setFilteredcourse(
+        tempCourses.filter(
+          item=>item.courseTitle.toLowerCase().includes(input.toLowerCase())
+        )
+      )
+      :
+      setFilteredcourse(tempCourses)
+    }
+  }, [allcourses,input])
   return (
-    <div>
-      <h1>Course List</h1>
-    </div>
+    <>
+      <div className='lg:px-36 sm:px-10 md:px-20 pt-20 relative'>
+        <div className='flex flex-col md:flex-row items-center justify-between gap-5 px-5'>
+          <div className=''>
+            <h1 className='text-4xl font-semibold'>Course List</h1>
+            <p className='text-balance p-1 cursor-pointer'><span onClick={() => navigate('/')} className='text-blue-600'>Home</span> / <span className='text-gray-500'>Course List</span></p>
+          </div>
+          <div>
+            <SearchBar data={input}/>
+          </div>
+        </div>
+        <div className='grid lg:grid-cols-4 md:grid-cols-2 gap-6 px-5'>
+          {
+            filteredCourse.map((course, id) => (
+              <CourseCard key={id} course={course} />
+            ))
+          }
+        </div>
+      </div>
+      <Footer />
+    </>
+
   )
 }
 
