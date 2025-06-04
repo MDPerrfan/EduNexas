0c166e6e765491a9a39c2e8ade396b7f9846389b
Mohammed Parves <mdperrfan@gmail.com>
2025-06-04 23:18:16 +0600

    loading animation

--- client/src/components/students/Loading.jsx
+++ client/src/components/students/Loading.jsx
@@ -2,9 +2,11 @@ import React from 'react'
 
 const Loading = () => {
   return (
-    <div>
-      <h1>Loading</h1>
-    </div>
+    <>
+      <div className="fixed inset-0 bg-black bg-opacity-50 flex items-center justify-center z-50">
+        <div className="w-12 h-12 border-4 border-t-blue-500 border-white rounded-full animate-spin"></div>
+      </div>
+    </>
   )
 }
 
