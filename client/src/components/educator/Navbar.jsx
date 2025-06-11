b96511b029c472ddda171ff44e75fa186ecd85cb
Mohammed Parves <mdperrfan@gmail.com>
2025-06-11 17:39:30 +0600

    Completed Sidebar

--- client/src/components/educator/Navbar.jsx
+++ client/src/components/educator/Navbar.jsx
@@ -6,11 +6,11 @@ const Navbar = () => {
   const educatorData = dummyEducatorData
   const {user}= useUser()
   return (
-    <div className='flex items-center justify-between px-4 md:px-8 border-b border-gray-500 py-3'>
+    <div className='flex items-center justify-between px-4 md:px-8 border-b border-gray-500 py-3 bg-blue-300/70'>
       <Link to='/'>
-            <img src={assets.logo} alt="logo" className='w-28 lg:w-32' />
+            <img src={assets.logo} alt="logo" className='w-24 lg:w-28' />
       </Link>
-      <div className='flex items-center gap-5 text-gray-500 relative'>
+      <div className='flex items-center gap-5 text-gray-700 relative'>
         <p>Hi! {user?user.fullName:'Developers'}</p>
         {user? <UserButton/>: <img className='max-w-8' src={assets.profile_img}/>}
       </div>
--- client/src/components/educator/Sidebar.jsx
+++ client/src/components/educator/Sidebar.jsx
@@ -1,12 +1,27 @@
-import React from 'react'
+import React, { useContext } from 'react'
+import { assets } from '../../assets/assets'
+import { AppContext } from '../../context/AppContext'
+import { NavLink } from 'react-router-dom'
 
 const Sidebar = () => {
+  const {isEducator}=useContext(AppContext)
   const menutItems=[
-    
+    {name:'Dashboard',path:'/educator',icon:assets.home_icon},
+    {name:'Add Course',path:'/educator/add-course',icon:assets.add_icon},
+    {name:'My Course',path:'/educator/my-courses',icon:assets.my_course_icon},
+    {name:'Student Enrolled',path:'/educator/student-enrolled',icon:assets.person_tick_icon}
+
+
+
   ]
   return (
-    <div>
-      <h1>Sidebar</h1>
+    <div className='w-16 md:w-64 flex flex-col min-h-screen py-2 border-r border-gray-500 text-base'>
+      {menutItems.map((item)=>(
+        <NavLink to={item.path} key={item.name} end={item.path==='/educator' } className={({isActive})=>`flex items-center md:flex-row flex-col md:justify-start justify-center py-3 md:px-10 gap-3 ${isActive? 'bg-orange-50 border-r-[6px] border-orange-500/90':'hover:bg-gray-100/90 border-r-[6px] border-white hover:border-gray-100/90'}`}>
+          <img src={item.icon} alt="" className='w-6 h-6'/>
+          <p className='md:block hidden text-center'>{item.name}</p>
+        </NavLink>
+      ))}
     </div>
   )
 }
