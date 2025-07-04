404beeea89e1e38aa99ec0d2fb0bbe326821b1cf
Mohammed Parves <mdperrfan@gmail.com>
2025-07-04 19:11:46 +0600

    Connected mongodb created user model and controller for clerk

--- server/server.js
+++ server/server.js
@@ -1,9 +1,12 @@
 import express from 'express'
 import cors from 'cors'
 import 'dotenv/config'
+import connectDB from './config/mongodb.js'
+import clerkWebhooks from './controllers/webhooks.js'
 
 
 const app = express()
+connectDB()
 
 app.use(cors({
 
@@ -12,8 +15,8 @@ app.use(cors({
 //Routes
 
 app.get('/', (req, res) => res.send("API working"))
-
-//port
+app.post('/clerk', express.json(), clerkWebhooks)
+    //port
 const PORT = process.env.PORT || 5000
 
 app.listen(PORT, () => {
