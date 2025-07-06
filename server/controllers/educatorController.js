9147bbceb712e0f2cdd7ed2e4f61c93c2e7583b3
Mohammed Parves <mdperrfan@gmail.com>
2025-07-06 12:57:35 +0600

    Added function to add course

--- server/controllers/educatorController.js
+++ server/controllers/educatorController.js
@@ -1,4 +1,6 @@
 import { clerkClient } from '@clerk/express'
+import Course from '../models/Course.js';
+import { v2 as cloudinary } from 'cloudinary'
 export const updateRoleEducator = async(req, res) => {
     try {
         const { userId } = req.auth();
@@ -8,6 +10,32 @@ export const updateRoleEducator = async(req, res) => {
             }
         })
         res.json({ success: true, message: 'You can now publish courses now.' })
+    } catch (error) {
+        res.json({ success: false, message: error.message })
+    }
+}
+
+//add new course
+
+export const addCourse = async(req, res) => {
+    try {
+        const { courseData } = req.body
+        const imageFile = req.imageFile
+        const educatorId = req.auth();
+        if (!imageFile) {
+            return res.json({ success: false, message: "Thumbnail is not attached" })
+
+
+        }
+        const parsedCourseData = await JSON.parse(courseData)
+        parsedCourseData.educator = educatorId
+
+        const newCourse = await Course.create(parsedCourseData)
+        const imageUpload = await cloudinary.uploader.upload(imageFile.path)
+        newCourse.courseThumbnail = imageUpload.secure_url
+
+        await newCourse.save()
+        res.json({ success: true, message: "Course Added" })
     } catch (error) {
         res.json({ success: false, message: error.message })
     }
